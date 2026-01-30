050e279cb4b5e636d01648c8a0a439bcfef3115c
John Finley <john.finley@gmail.com>
2026-01-30 15:05:56 -0600

    updated gpio usage

---
+++ include/gui.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "gui_button.h"
+#include "gui_label.h"
+#include "gui_macros.h"
+#include "gui_number.h"
+#include "gui_page.h"
+#include "gui_slider.h"
--- test/fb_gpio_cfg.h
+++ test/fb_gpio_cfg.h
@@ -2,65 +2,28 @@
 
 #include "hardware/spi.h"
 
-#if 1
-
-//               +------| USB |------+
-//             1 | D0       VBUS_OUT | 40
-//             2 | D1        VSYS_IO | 39
-//             3 | GND           GND | 38
-//             4 | D2         3V3_EN | 37
-//             5 | D3        3V3_OUT | 36
-//  (ts) SDA   6 | D4           AREF | 35
-//  (ts) SCL   7 | D5         A2/D28 | 34
-//             8 | GND           GND | 33
-//  (ts) RST   9 | D6         A1/D27 | 32
-//  (ts) INT  10 | D7         A0/D26 | 31
-//            11 | D8            RUN | 30
-//            12 | D9            D22 | 29  LED  (fb)
-//            13 | GND           GND | 28
-//            14 | D10           D21 | 27  RST  (fb)
-//            15 | D11           D20 | 26  CD   (fb)
-//            16 | D12           D19 | 25  MOSI (fb)
-//            17 | D13           D18 | 24  SCK  (fb)
-//            18 | GND           GND | 23
-//            19 | D14           D17 | 22  CS   (fb)
-//            20 | D15           D16 | 21  MISO (fb)
-//               +-------------------+
-
-constexpr int fb_spi_miso_gpio = 16;
-constexpr int fb_spi_mosi_gpio = 19;
-constexpr int fb_spi_clk_gpio = 18;
-constexpr int fb_spi_cs_gpio = 17;
-spi_inst_t* const fb_spi_inst = spi0;
-
-constexpr int fb_cd_gpio = 20;
-constexpr int fb_rst_gpio = 21;
-constexpr int fb_led_gpio = 22;
-
-#else
-
-//               +------| USB |------+
-//  (ts) SDA   1 | D0       VBUS_OUT | 40
-//  (ts) SCL   2 | D1        VSYS_IO | 39
-//             3 | GND           GND | 38
-//  (ts) RST   4 | D2         3V3_EN | 37
-//  (ts) INT   5 | D3        3V3_OUT | 36
-//  (fb) MISO  6 | D4           AREF | 35
-//  (fb) CS    7 | D5         A2/D28 | 34  CS   (dcc)
-//             8 | GND           GND | 33
-//  (fb) SCK   9 | D6         A1/D27 | 32  11
-//  (fb) MOSI 10 | D7         A0/D26 | 31  10
-//  (fb) CD   11 | D8            RUN | 30
-//  (fb) RST  12 | D9            D22 | 29  9
-//            13 | GND           GND | 28
-// (dcc) PWR  14 | D10           D21 | 27  8
-// (dcc) SIG  15 | D11           D20 | 26  7
-//  (fb) LED  16 | D12           D19 | 25  6
-// (dcc) RXD  17 | D13           D18 | 24  5
-//            18 | GND           GND | 23
-//         1  19 | D14           D17 | 22  4
-//         2  20 | D15           D16 | 21  3
-//               +-------------------+
+//                     +-----| USB |-----+
+//  (ts)  SDA X     D0 | 1            40 | VBUS_OUT
+//  (ts)  SCL X     D1 | 2            39 | VSYS_IO
+//                 GND | 3            38 | GND
+//  (ts)  RST       D2 | 4            37 | 3V3_EN
+//  (ts)  INT       D3 | 5            36 | 3V3_OUT
+//  (fb) MISO       D4 | 6            35 | AREF
+//  (fb)   CS       D5 | 7            34 | A2/D28 X CS   (dcc)
+//                 GND | 8            33 | GND
+//  (fb)  SCK       D6 | 9            32 | A1/D27        (11)
+//  (fb) MOSI       D7 | 10           31 | A0/D26        (10)   CS (dcc)
+//  (fb)   CD       D8 | 11           30 | RUN
+//  (fb)  RST       D9 | 12           29 | D22           (9)
+//                 GND | 13           28 | GND
+// (dcc)  PWR X    D10 | 14           27 | D21           (8)    SCL (ts)
+// (dcc)  SIG X    D11 | 15           26 | D20           (7)    SDA (ts)
+//  (fb)  LED      D12 | 16           25 | D19           (6)    SIG (dcc)
+// (dcc)  RXD X    D13 | 17           24 | D18           (5)    PWR (dcc)
+//                 GND | 18           23 | GND
+//  (1)            D14 | 19           22 | D17           (4)    RXD (dcc)
+//  (2)            D15 | 20           21 | D16           (3)
+//                     +-----------------+
 
 constexpr int fb_spi_miso_gpio = 4;
 constexpr int fb_spi_mosi_gpio = 7;
@@ -71,5 +34,3 @@ spi_inst_t* const fb_spi_inst = spi0;
 constexpr int fb_cd_gpio = 8;
 constexpr int fb_rst_gpio = 9;
 constexpr int fb_led_gpio = 12;
-
-#endif
